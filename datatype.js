ab8ff05238e0008f7260011654f4f0122995c6ee
anandtathe5 <166648054+anandtathe5@users.noreply.github.com>
2024-07-31 17:09:55 +0530

    Add files via upload

---
+++ datatype.js
@@ -0,0 +1,34 @@
+// Variables initialization
+let myFullName;
+console.log(`My Full Name Value Before initialization:: ${myFullName}`);
+
+let firstName = "Anand";
+let lastName = "Tathe";
+
+// Initializing full name
+myFullName = `${firstName} ${lastName}`;
+console.log(`My First Name and Last Name After initialization is: ${myFullName}`);
+
+// Updating full name
+myFullName = `${firstName} Pramod ${lastName}`;
+console.log(`After Updating My Full Name Is ${myFullName}`);
+
+console.log('I Love only Js');
+
+console.log('===');
+
+// Other variables
+let age = 19;
+let address = 'Nashik';
+let pincode = 422003;
+let gender = 'Male';
+let marriedStatus = false;
+
+// Printing variables
+console.log(`My First Name is: ${firstName}`);
+console.log(`My Last Name is: ${lastName}`);
+console.log(`My Age Name is: ${age}`);
+console.log(`My Address is: ${address}`);
+console.log(`My Pincode is: ${pincode}`);
+console.log(`My Gender is: ${gender}`);
+console.log(`My Married Status is: ${marriedStatus}`);
